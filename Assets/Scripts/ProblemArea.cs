8d86c5a550cdedca5bdb61bb1c1765ee7602ef41
Jim Verhaeghe <12163046+jpverhaeghe@users.noreply.github.com>
2023-01-15 13:51:29 +0000

    Added new level Added new player Added all puzzles to new level Adjusted materials for puzzles to be controlled by the puzzle area script so there can be differnt ones Added problem prefabs that can be put anywhere now.

--- Assets/Scripts/ProblemArea.cs
+++ Assets/Scripts/ProblemArea.cs
@@ -7,22 +7,20 @@ using static GameManager;
 public class ProblemArea : MonoBehaviour
 {
     // Set up serialized variables for this mini game
-    [Header("Character controller")]
-    [SerializeField] CharacterControllerRB characterController;
-    [SerializeField] GameManager gameManager;
-    [SerializeField] ProblemTypes problemType;
+    [Header("Game connections")]
+    [SerializeField] CharacterControllerRB characterController;         // a connection to the character controller
+    [SerializeField] GameManager gameManager;                           // a connection to the game manager
+
+    [Header("Problem material and area")]
+    [SerializeField] public Material problemMaterial;                   // the material to set up when there is a problem
+    [SerializeField] ProblemTypes problemType;                          // the problem type that this is
 
     // public variables available to other scripts
     public GameObject problemToSolve;
     public bool hasProblem = false;
 
-    /// <summary>
-    /// Start is called before the first frame update
-    /// </summary>
-    void Start()
-    {
-        
-    } // end Start
+    // private variables
+    private Material originalMaterial;                                  // the original material of the problem area
 
     /// <summary>
     /// Update is called once per frame
@@ -33,7 +31,7 @@ public class ProblemArea : MonoBehaviour
         {
             bool startPuzzle = false;
 
-            // some things need to happen if it is a UI puzzle type
+            // Set up the problem based on specific character interactions
             switch(problemType)
             {
                 case ProblemTypes.FIX_WIRES:
@@ -68,6 +66,7 @@ public class ProblemArea : MonoBehaviour
                     break;
             }
 
+            // start the puzzle if it is time
             if (startPuzzle)
             {
                 // set the problem as active
@@ -83,4 +82,53 @@ public class ProblemArea : MonoBehaviour
         }
         
     } // end Update
+
+    /// <summary>
+    /// Updates the material based on the state of the problem area
+    /// </summary>
+    public void UpdateMaterial()
+    {
+        MeshRenderer rendererToUpdate;
+
+        // some things need to happen if it is a UI puzzle type
+        switch (problemType)
+        {
+            // code breaker and simon says are unique as they use the same game object, get their parent material
+            case ProblemTypes.CODE_BREAKER:
+            case ProblemTypes.SIMON_SAYS:
+                rendererToUpdate = gameObject.GetComponentInParent<MeshRenderer>();
+                break;
+
+            // all others just use the game object they are attached to
+            default:
+                rendererToUpdate = gameObject.GetComponent<MeshRenderer>();
+                break;
+        }
+
+        // if we are starting the problem, then update the material to show there is a problem with this game object
+        if (hasProblem)
+        {
+            // the fire problem has a special floor material to turn on so do that
+            if (problemType == ProblemTypes.FIRE)
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+
+            originalMaterial = rendererToUpdate.material;
+            rendererToUpdate.material = problemMaterial;
+        }
+        // otherwise set it back to normal
+        else
+        {
+            // the fire problem has a special floor material to turn off so do that
+            if (problemType == ProblemTypes.FIRE)
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+
+            rendererToUpdate.material = originalMaterial;
+        }
+
+    } // end UpdateMaterial
+
 }
